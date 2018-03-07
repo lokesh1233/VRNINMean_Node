4ba3fbdc1beef5a760afe3a4145afaf263b3372f
lokesh <yell.nagalokesh@gmail.com>
2018-03-07 13:47:47 +0530

    bundle prepare

--- server.js
+++ server.js
@@ -15,7 +15,7 @@
     // configuration ===============================================================
     mongoose.connect(database.url);     // connect to mongoDB database on modulus.io
 
-    app.use(express.static(__dirname + '/public/src'));                 // set the static files location /public/img will be /img for users
+    app.use(express.static(__dirname + '/dist'));                 // set the static files location /public/img will be /img for users
     app.use(morgan('dev'));                                         // log every request to the console
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
