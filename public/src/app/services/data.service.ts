5f214b7afa8975aa7f68966b5b47e83e47c8c392
Bhaskar <bhaskarmuthineni@gmail.com>
2018-03-15 15:54:02 +0530

    service file change for vrn checkout

--- public/src/app/services/data.service.ts
+++ public/src/app/services/data.service.ts
@@ -59,4 +59,10 @@ export class DataService {
 			// headers:new Headers({'Content-Type':'application/json'})
 		}).map(res => res.json());
 	}
+
+	createVRNCheckOut(id, data){
+		return this.http.post('/createVRNCheckOut/'+id, data,{
+				// headers:new Headers({'Content-Type':'application/json'})
+			}).map(res => res.json());
+		}
 }
